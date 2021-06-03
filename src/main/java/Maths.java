257165181ea4df78055a3285f9a62a1129437491
serinamai <phuongdao.mt@gmail.com>
2021-06-03 18:25:18 +0700

    init

---
+++ src/main/java/Maths.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+public class Maths {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int multiple(int a, int b){
+        return a * b;
+    }
+
+
+
+}
---
+++ src/test/java/MathsTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+public class MathsTest {
+    public Maths maths = new Maths();
+
+    @Test
+    public void addition() {
+        int a = 5;
+        int b = 3;
+        assert maths.add(a, b) == 8;
+    }
+
+
+    @Test
+    public void subtraction() {
+        int a = 5;
+        int b = 2;
+        assert maths.subtract(a, b) == 3;
+    }
+
+    @Test
+    public void division() {
+        int a = 4;
+        int b = 2;
+        assert maths.divide(a, b) == 2;
+    }
+
+    @Test
+    public void multiple() {
+        int a = 4;
+        int b = 2;
+        assert maths.multiple(a, b) == 8;
+    }
+}
